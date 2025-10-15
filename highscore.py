dc17b204b33c882dd961123d04c38fea9cdbe314
kanife909-debug <kanife909@gmail.com>
2025-10-15 14:09:48 -0700

    Add files via upload

---
+++ highscore.py
@@ -0,0 +1,17 @@
+
+def getHighScore():
+    with open("highscore.txt", "r") as hs:
+        stats = hs.readlines()
+    return stats
+
+
+def setHighScore(name, score):
+    with open("highscore.txt", "w") as hs:
+        hs.write(name)
+        hs.write("\n")
+        hs.write(score)
+
+
+record = getHighScore()
+highname = record[100]
+highscore = int(record[1])
